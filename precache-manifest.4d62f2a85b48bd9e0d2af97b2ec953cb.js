c478eb50e5f8be138d966fd5b955cad80b730fa7
Kirill Ivashkin <kirillkus@yandex.ru>
2021-05-13 09:49:19 +0300

    Updates

---
+++ precache-manifest.4d62f2a85b48bd9e0d2af97b2ec953cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9db350d3b79b8790d8f",
+    "url": "/re-store-practice/static/css/main.afd5fcf4.chunk.css"
+  },
+  {
+    "revision": "f9db350d3b79b8790d8f",
+    "url": "/re-store-practice/static/js/main.f9db350d.chunk.js"
+  },
+  {
+    "revision": "cc74583b96b13bbbaadc",
+    "url": "/re-store-practice/static/js/1.cc74583b.chunk.js"
+  },
+  {
+    "revision": "63033aa6d2fe735da545",
+    "url": "/re-store-practice/static/js/runtime~main.63033aa6.js"
+  },
+  {
+    "revision": "30297d43c859db9602489e3896103a9b",
+    "url": "/re-store-practice/index.html"
+  }
+];
